3d44e55032f6af571e228553951528ca450216ac
Manas-Upadhyay45 <manasupadhyay45@gmail.com>
2026-02-15 11:41:02 +0530

    update

--- frontend/js/firebase-config.js
+++ frontend/js/firebase-config.js
@@ -1,15 +1,21 @@
-// Firebase configuration (copy from Firebase Console)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "oMAK6fIjew39cOvvB8Lo",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAom7HOb0uDR9U9WYGIJk5H2Hdf8DQ-nEs",
+  authDomain: "build-data-15c3a.firebaseapp.com",
+  projectId: "build-data-15c3a",
+  storageBucket: "build-data-15c3a.firebasestorage.app",
+  messagingSenderId: "756232059529",
+  appId: "1:756232059529:web:76d8d2812163d6fbc6a458",
+  measurementId: "G-JM10ELQE9G"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// 🔥 THIS LINE FIXES YOUR ERROR
-const cfFirebaseAuth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
